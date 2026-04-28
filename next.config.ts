87843d6488205832a4fba211b81c74d453994259
Thangga7 <beencules@gmail.com>
2026-04-28 16:03:06 +0700

    hi

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const rootDir = fileURLToPath(new URL(".", import.meta.url));
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "192.168.10.11",
-    "*.ngrok-free.app",
-    "*"
+    "9aea-42-114-112-109.ngrok-free.app",
+    "8767-42-114-112-109.ngrok-free.app"
   ],
   images: {
     remotePatterns: [
